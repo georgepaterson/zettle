ca410cdfb878f26f4b731479c707165b6522adf5
George Paterson <paterson.george@gmail.com>
2021-12-11 15:47:34 +0000

    Connecting to Mongo.

--- src/server.js
+++ src/server.js
@@ -1,6 +1,16 @@
 
 const express = require('express');
 const app = express();
+require('dotenv').config();
+const mongoose = require("mongoose");
+const cron = require('node-cron');
+
+const db = process.env.MONGODB_URI;
+
+mongoose
+  .connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
