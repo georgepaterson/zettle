e6bb720eeb248045bc3a960f781f2ed0e2d497e2
George Paterson <paterson.george@gmail.com>
2021-12-12 16:31:08 +0000

    Filtering products to remove based on category.

--- src/controllers/remove.js
+++ src/controllers/remove.js
@@ -18,20 +18,59 @@ async function getZettle(token) {
             success: true,
             data: response.data
         }
-    } catch (e) {
+    } catch (error) {
         return {
             success: false,
-            data: 'FAILURE TO GET ORDER FOR IZETTLE'
+            data: `Failed to get Zettle products:: ${error}`
         }
     }
 }
 
-async function remove(){
+async function filterZettle(products) {
+    try {
+        const list = [];
+        products.forEach((product) => {
+            if(product.category) {
+                if(!product.category.name.includes('Service Fees')) {
+                    list.push(product.uuid);
+                }
+            } else {
+                list.push(product.uuid);
+            }
+        })
+        return {
+            success: true,
+            data: list
+        }
+    } catch (error) {
+        return {
+            success: false,
+            data: `Failed to filter Zettle product: ${error}`
+        }
+    }
+}
+
+async function postZettle(list) {
+    try {
+        console.log(list);
+        
+        return {
+            success: true,
+            data: list
+        }
+    } catch (error) {
+        return {
+            success: false,
+            data: `Failed to remove Zettle product: ${error}`
+        }
+    }
+}
+
+async function remove() {
     const access = await token();
     const products = await getZettle(access.data.access_token);
-    console.log("remove");
-    console.log(products);
-
+    const filtered = await filterZettle(products.data);
+    const removed = await postZettle(filtered.data);
 };
 
 module.exports = remove;
